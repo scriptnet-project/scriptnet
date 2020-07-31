e07d84f5e9150853f4401e691985f300b9bc1340
Steve Mckellar <steve@wwqrd.uk>
2020-07-31 16:00:05 +0100

    Restored persistor

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-// import { PersistGate } from 'redux-persist/integration/react';
+import { PersistGate } from 'redux-persist/integration/react';
 import getStore from 'Renderer/store';
 import 'Renderer/index.scss';
 import App from 'Components/App';
 
 const { store, persistor } = getStore();
-  //   <PersistGate loading={null} persistor={persistor}>
 
-  //   </PersistGate>
 ReactDOM.render((
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 ), document.getElementById('app'));
