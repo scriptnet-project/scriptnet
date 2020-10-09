a6c54a9d22e85c2ebd1e88e9766477aef82c24f0
Steve Mckellar <steve@wwqrd.uk>
2020-10-09 11:38:17 +0100

    Render Visualisation component after scale animation completes

--- src/renderer/components/VisualisationScreen/VisualisationScreen.js
+++ src/renderer/components/VisualisationScreen/VisualisationScreen.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import Screen from 'Components/Screen';
 import Visualisation from 'Components/VisualisationScreen/Visualisation';
 import ControlBar from 'Components/VisualisationScreen/ControlBar';
 import PanelManager from 'Components/VisualisationScreen/PanelManager';
 import TopCommandBar from './CommandBar';
+import useCytoscape from '../../hooks/useCytoscape';
 
 const VisualisationScreen = () => {
+  const [cy] = useCytoscape();
+  const [state, setState] = useState({ animationComplete: false });
+
+  const handleAnimationComplete = useCallback(() => {
+    setState({ animationComplete: true });
+  }, [cy]);
+
   return (
-    <Screen>
+    <Screen onAnimationComplete={handleAnimationComplete}>
       <TopCommandBar />
       <div style={{ flex: '1 auto', display: 'flex' }}>
-        <Visualisation/>
+        { state.animationComplete && <Visualisation/> }
         <PanelManager/>
       </div>
       <ControlBar />
