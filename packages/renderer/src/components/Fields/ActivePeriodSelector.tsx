e08a2f3498c43eb04f14881135d12c9cb4fc972b
Joshua Melville <joshua.melville@northwestern.edu>
2022-04-13 12:06:09 +0200

    correct styling for active period selector

--- packages/renderer/src/components/Fields/ActivePeriodSelector.tsx
+++ packages/renderer/src/components/Fields/ActivePeriodSelector.tsx
@@ -1,6 +1,28 @@
 import { useState, useEffect } from 'react';
 import { useField, Form, FormikProps, Formik } from 'formik';
-import { DialogType, ActionButton, BaseButton, DefaultButton, DetailsList, Dialog, DialogFooter, getTheme, Label, Modal, PrimaryButton, TextField, DatePicker, Selection, SelectionMode, IColumn, Stack } from '@fluentui/react';
+import {
+  DialogType,
+  ActionButton,
+  BaseButton,
+  DefaultButton,
+  DetailsList,
+  Dialog,
+  DialogFooter,
+  getTheme,
+  Label,
+  Modal,
+  PrimaryButton,
+  TextField,
+  DatePicker,
+  Selection,
+  SelectionMode,
+  IColumn,
+  Stack,
+  IDetailsListStyles,
+  DetailsListLayoutMode,
+  ConstrainMode,
+  Text,
+} from '@fluentui/react';
 
 function formatDate(date) {
     var d = new Date(date),
@@ -22,13 +44,7 @@ const onFormatDate = (date?: Date): string => {
 
 const ActivePeriodSelector = (props) => {
   const theme = getTheme();
-  const [items, setItems] = useState([
-    {
-      key: 1,
-      start: new Date('2020-01-01'),
-      end: new Date('2020-01-02'),
-    },
-]);
+  const [items, setItems] = useState([]);
   const [field, meta, helpers] = useField(props.name);
   const [selectedItem, setSelectedItem] = useState(undefined);
 
@@ -44,29 +60,22 @@ const ActivePeriodSelector = (props) => {
   }, [selectedItem])
 
   const { value } = meta;
-
   const { setValue } = helpers;
-
-  const COL_WIDTH = 145;
 
   const columns: IColumn[] = [
     {
       key: 'startDate',
       name: 'Start Date',
       fieldName: 'start',
-      minWidth: COL_WIDTH,
-      isResizable: false,
-      isCollapsible: false,
-      isPadded: false,
+      minWidth: 0,
+      maxWidth: 0,
     },
     {
       key: 'endDate',
       name: 'End Date',
       fieldName: 'end',
-      minWidth: COL_WIDTH,
-      isResizable: false,
-      isCollapsible: false,
-      isPadded: false,
+      minWidth: 0,
+      maxWidth: 0,
     },
   ];
 
@@ -75,49 +84,88 @@ const ActivePeriodSelector = (props) => {
       ...items,
       {
         key: items.length + 1,
-        start: new Date(),
-        end: new Date(),
+        start: null,
+        end: null,
       },
     ]);
   }
 
   console.log({ meta, helpers, field });
 
+  const detailsStyles: IDetailsListStyles = {
+    root: {
+      '.ms-DetailsRow-cell, .ms-DetailsHeader-cell': {
+        width: '50% !important',
+      },
+      '.ms-DetailsRow-fields, .ms-DetailsRow': {
+        width: '100%',
+      },
+    },
+    headerWrapper: {
+      '.ms-DetailsHeader': {
+        padding: 0,
+      },
+    },
+    focusZone: undefined,
+    contentWrapper: undefined
+  };
+
+  const handleDeleteSelectedItem = () => {
+    const newItems = items.filter(item => item.key !== selectedItem.key);
+    setItems(newItems);
+    setSelectedItem(undefined);
+  }
+
+
   return (
-    <Stack
-    >
-      <Label>{props.label}</Label>
-      <DetailsList
-        items={items}
-        columns={columns}
-        selection={selection}
-        selectionMode={SelectionMode.single}
-        checkboxVisibility={2}
-        onRenderItemColumn={(item, index, column) => {
-          console.log(item, index, column);
-          const data = item[column.fieldName];
-          return (
-            <DatePicker value={data} />
-          )
-        }}
-      />
-      <Stack
-        horizontal
-        tokens={{
-          padding: '10px 0px 0px 0px',
-          childrenGap: '10px',
-        }}
-      >
-        <PrimaryButton iconProps={{ iconName: 'add'}} onClick={handleAddNew}>Add</PrimaryButton>
-        { selectedItem && (
-          <PrimaryButton
-            iconProps={{ iconName: 'delete', children: theme.palette.red}}
-            style={{backgroundColor:theme.palette.red, color:'white'}}
-            >
-              Delete
-            </PrimaryButton>
+    <Stack>
+        <Label>{props.label}</Label>
+        <div
+          style={{
+            border: `1px solid ${theme.semanticColors.inputBorder}`,
+          }}
+        >
+        <DetailsList
+          theme={theme}
+          items={items}
+          columns={columns}
+          selection={selection}
+          selectionMode={SelectionMode.single}
+          checkboxVisibility={2}
+          layoutMode={DetailsListLayoutMode.fixedColumns}
+          constrainMode={ConstrainMode.horizontalConstrained}
+          onRenderItemColumn={(item, index, { fieldName }: IColumn) => {
+            const data = item[fieldName];
+            return (
+              <DatePicker value={data} />
+            )
+          }}
+          styles={detailsStyles}
+        />
+        { !items.length && (
+          <Stack horizontalAlign='center' style={{ paddingBottom: theme.spacing.m }}>
+            <Text>No periods of involvement defined.</Text>
+          </Stack>
         )}
-      </Stack>
+        <Stack
+          horizontal
+          tokens={{
+            padding: theme.spacing.s1,
+            childrenGap: theme.spacing.s1,
+          }}
+        >
+          <PrimaryButton iconProps={{ iconName: 'add'}} onClick={handleAddNew}>Add</PrimaryButton>
+          { selectedItem && (
+            <PrimaryButton
+              iconProps={{ iconName: 'delete', children: theme.palette.red}}
+              style={{backgroundColor:theme.palette.red, color:'white'}}
+              onClick={handleDeleteSelectedItem}
+              >
+                Delete
+              </PrimaryButton>
+          )}
+        </Stack>
+      </div>
     </Stack>
   )
 }
