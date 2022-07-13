347ef7760e96830d39e9804b3c997ac3725f161e
Joshua Melville <joshua.melville@northwestern.edu>
2022-07-13 22:38:42 +0200

    complete implementation of involvement slider

--- packages/renderer/src/components/Legend/InvolvementSlider.tsx
+++ packages/renderer/src/components/Legend/InvolvementSlider.tsx
@@ -4,6 +4,7 @@ import { motion } from 'framer-motion';
 import { useDispatch, useSelector } from 'react-redux';
 import { Slider } from '@fluentui/react';
 import { isEmpty } from 'lodash';
+import { CollectionGraphManipulation, NodeSingular } from 'cytoscape';
 
 let maxPossibleDate = new Date(8640000000000000);
 let minPossibleDate = new Date(-8640000000000000);
@@ -39,14 +40,17 @@ const getInvolvementRange = (involvements: Involvement[]) => {
 //  - 04/2020 - 04/2020 - NO
 
 const nodeHasInvolvementWithinRange = (involvements: Involvement[], start: Date, end: Date) => {
-  return involvements.some(involvement => {
+  return involvements.some((involvement: Involvement) => {
     const startDate = involvement.start ? new Date(involvement.start) : minPossibleDate;
     const endDate = involvement.end ? new Date(involvement.end) : maxPossibleDate;
-    return startDate <= start && endDate >= end;
+
+    // startDate is after start OR
+    // endDate is after start date and before end date
+    return (startDate >= start && startDate <= end) || (endDate >= start && endDate <= end);
   });
 }
 
-let filteredElements;
+let filteredElements: CollectionGraphManipulation | null;
 
 const InvolvementSlider = () => {
   const { cy } = useCytoscape();
@@ -54,7 +58,8 @@ const InvolvementSlider = () => {
     runLayout,
   } = useCytoscapeActions();
 
-  const [ dates, setDates ] = useState([]);
+  const [ dates, setDates ] = useState([] as Date[]);
+  const [sliderValue, setSliderValue] = useState([0, 0]);
 
   useEffect(() => {
     if(!cy) return;
@@ -65,9 +70,10 @@ const InvolvementSlider = () => {
       return;
     }
 
-    const newDates = [];
+    const newDates: Date[] = [];
 
-    cy.current.nodes().map((node) => {
+    // Put all dates in all involvements into an array
+    cy.current.nodes().forEach((node: NodeSingular) => {
       const involvements = node.data('involvements');
 
       if (!involvements) {
@@ -76,99 +82,95 @@ const InvolvementSlider = () => {
 
       involvements.forEach((involvement: Involvement) => {
         if (involvement.start) {
-          newDates.push(involvement.start);
+          newDates.push(new Date(involvement.start));
         }
 
         if (involvement.end) {
-          newDates.push(involvement.end);
+          newDates.push(new Date(involvement.end));
         }
       });
     });
 
     // Make sure we have unique dates
-    const uniqueDates = new Set(newDates);
+    const uniqueDates = [...new Set(newDates)];
 
     // Sort dates
-    const sortedDates = [...uniqueDates].sort((a, b) => {
-      return new Date(a) - new Date(b);
-    });
+    const sortedDates = uniqueDates.sort((a, b) => a.getTime() - b.getTime());
 
     const datesWithBounds = [
       minPossibleDate,
       ...sortedDates,
       maxPossibleDate,
-    ]
+    ];
 
     setDates(datesWithBounds);
-
+    setSliderValue(([lower]) => ([lower, datesWithBounds.length - 1]));
+    console.table(datesWithBounds);
   }, [cy.current]);
 
-  const [sliderValue, setSliderValue] = useState([0, 0]);
-
-  // Update the upper slider value when the dates change
-  useEffect(() => {
-    if (isEmpty(dates)) {
+  // Change handler
+  const onChange = (_: any, value: number[]) => {
+    // Reject if upper value is within 1 of lower value
+    if (value[1] - value[0] < 1) {
       return;
     }
 
-    setSliderValue(value => [value[0], dates.length - 1]);
-  }, [dates]);
+    setSliderValue(value); // Update the slider value
 
-  const onChange = useCallback((event, value) => {
+    const [lowerValueIndex, upperValueIndex] = value;
+    // Start with the full unfiltered network, by restoring any previously filtered items
     if (filteredElements) {
       filteredElements.restore();
       filteredElements = null;
     }
 
-    setSliderValue(value);
-
-    const [start, end] = value;
-
     // Don't filter at all if we are at "all" on both sides
-    if (value[0] === 0 && value[1] === dates.length - 1) {
+    if (lowerValueIndex === 0 && upperValueIndex === dates.length - 1) {
       return;
     }
 
-    const minSliderDate = value[0] === 0 ? maxPossibleDate : new Date(dates[start]);
-    const maxSliderDate = value[1] === dates.length -1 ? minPossibleDate : new Date(dates[end]);
+    // Get the dates for the lower and upper slider values based on the index in the dates array
+    const lowerValueDate = lowerValueIndex === 0 ? minPossibleDate : dates[lowerValueIndex];
+    const upperValueDate = upperValueIndex === dates.length -1 ? maxPossibleDate : dates[upperValueIndex];
 
     // Filter nodes whose involvements don't intersect the min and max dates
-    filteredElements = cy.current.nodes().filter((node) => {
+    filteredElements = cy.current.nodes().filter((node: NodeSingular) => {
       const nodeInvolvements = node.data('involvements');
 
       // Remove nodes with no involvements
       if (!nodeInvolvements) {
-        return false;
-      }
-
-      const involvementRange = getInvolvementRange(nodeInvolvements);
-
-      const overlap = nodeHasInvolvementWithinRange(nodeInvolvements, minSliderDate, maxSliderDate);
-
-      if (overlap) {
         return true;
       }
 
-      return false;
+      const overlap = nodeHasInvolvementWithinRange(nodeInvolvements, lowerValueDate, upperValueDate);
+
+      if (overlap) {
+        return false;
+      }
+
+      return true;
     }).remove();
 
     runLayout();
-  }, [cy, dates, sliderValue]);
+  };
 
-  const formatValue = useCallback((value) => {
+  // Function to format the slider values in YYYY-MM-DD format
+  const formatValue = (value: number) => {
     if (isEmpty(dates)) {
       return '';
     }
 
-    if (value === 0 || value === dates.length - 1) {
-      return 'All';
+    if (value === 0) {
+      return 'All Past';
     }
 
-    const date = new Date(dates[value]);
-    return date.toISOString().slice(0, 10);
-  }, [dates]);
+    if (value === dates.length - 1) {
+      return 'All Future';
+    }
 
-  console.log(dates);
+    const date = dates[value];
+    return date.toLocaleDateString();
+  };
 
   return (
     <motion.div
